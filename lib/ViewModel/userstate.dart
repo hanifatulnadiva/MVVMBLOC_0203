e4eb587642a164c6858190a655142767ce8cdf7e
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2026-04-17 17:02:11 +0700

    add class  Userstate untuk menyimpan nama, email

---
+++ lib/ViewModel/userstate.dart
@@ -0,0 +1,10 @@
+import 'package:mvvmbloc_0203/Model/user.dart';
+
+class Userstate {
+  final String nama;
+  final String email;
+  Userstate({
+    this.nama = "",
+    this.email = "",
+  });
+}
