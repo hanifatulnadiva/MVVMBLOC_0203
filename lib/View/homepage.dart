279eaee3bbf0cb11cb7ac434c530ce88a069ed8a
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2026-04-17 17:44:39 +0700

    menambahkan body yang berisi column yang ada Textfield untuk nama,  jika terjadi perubahan  maka data akan kirim event NamaChanged ke UserBloc untuk mengelola dan update nama

--- lib/View/homepage.dart
+++ lib/View/homepage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mvvmbloc_0203/ViewModel/userbloc.dart';
+import 'package:mvvmbloc_0203/ViewModel/userevent.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,6 +10,18 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("MVVM with BloC Example")),
+      body:Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: "masukkan Nama"),
+              onChanged: (value){
+                context.read<Userbloc>().add(NamaChanged(value));
+              },
+            )
+          ],
+        ),)
     );
   }
 }
