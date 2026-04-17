9fa0c1e7217132c27217b7889c1a16cbc410bbb0
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2026-04-17 17:35:58 +0700

    struktur halaman menggunakan scaffold, dibagian appbar ada title untuk halaman pagenya

--- lib/View/homepage.dart
+++ lib/View/homepage.dart
@@ -1,8 +1,12 @@
+import 'package:flutter/material.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: Text("MVVM with BloC Example")),
+    );
   }
 }
